76c4e32ba0344b79e3c5293bb55b0aa0a0ec362a
LAPTOP-VM1PS42M\Daniel <daniel.hong@essoft.com.tw>
2022-08-30 14:55:26 +0800

    20220830_Vue JS 3 Tutorial for Beginners #6 - Build a Reaction Timer Game https://www.youtube.com/watch?v=bc6czIBLKTg&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=6

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,43 @@
 <template>
-    <div class="block">click me</div>
+    <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 
 <script>
     export default {
-        props: ['delay']
+        props: ['delay'],
+        data(){
+            return {
+                showBlock: false,
+                timer: null,
+                reactionTime: 0
+            }
+        },
+        mounted() {
+            //console.log('component mounted');
+            setTimeout(() => {
+                this.showBlock = true
+                //console.log(this.delay);
+                this.startTimer()
+            }, this.delay)
+        },
+        methods: {
+            startTimer() {
+                this.timer = setInterval(() => {
+                    this.reactionTime += 10
+                }, 10)
+            },
+            stopTimer() {
+                clearInterval(this.timer)
+                this.$emit('end', this.reactionTime)
+                console.log(this.reactionTime)
+            }
+        }
+        // updated() {
+        //     console.log('compoent update');
+        // },
+        // unmounted() {
+        //     console.log('unmounted');
+        // },
     };
 </script>
 
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,32 @@
-<template></template>
+<template>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank"> {{ rank }}</p>
+</template>
 
-<script></script>
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250){
+            this.rank = 'Ninja Fingers'
+        }else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        }else {
+            this.rank = 'snail pace..'
+        }
+    },
+};
+</script>
 
-<style scoped></style>
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
